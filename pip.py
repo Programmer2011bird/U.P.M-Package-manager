b0274df50127f4f4cee13c80f796725ec798ba40
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-02 16:26:34 +0330

    checking python-os compatibility in versions

--- pip.py
+++ pip.py
@@ -14,7 +14,7 @@ class pip_handler:
        
         self.LATEST_VERSION: str = str(self.RESPONSE_JSON["info"]["version"])
         self.release: list = list(self.RESPONSE_JSON["releases"][self.LATEST_VERSION])
-        print(self.release)
+        # print(self.release)
         self.desiredInfo: list[dict[str, str]] = []
 
         for _, release_info in enumerate(self.release):
@@ -25,12 +25,9 @@ class pip_handler:
         self.currentPYversion = f"cp{sys.version_info.major}{sys.version_info.minor}"
         self.check_compatibility(self.currentOs, self.currentPYversion)
 
-
     def check_compatibility(self, currentOs: str, currentPYversion: str) -> None:
         for _, info in enumerate(self.desiredInfo):
-            print(info)
             if (currentPYversion in info["pythonVersion"] or "py3" in info["pythonVersion"]) and (currentOs in info["fileName"] or "any" in info["fileName"].replace(".", "-").split("-")):
-                print(info)
                 print("compatible")
 
 
