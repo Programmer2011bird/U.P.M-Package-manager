e2f4f379d08977b632207138e4f84ef1c30e61fe
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-02 16:25:21 +0330

    checking python-os compatibility in versions

--- pip.py
+++ pip.py
@@ -1,30 +1,42 @@
+import platform
 import requests
+import sys
 
 
 class pip_handler:
     def __init__(self, packageName: str) -> None:
         self.URL: str = f"https://pypi.org/pypi/{packageName}/json"
+        self.download()
         
+    def download(self) -> None:
         self.RESPONSE: requests.Response = requests.get(self.URL)
         self.RESPONSE_JSON: dict = dict(self.RESPONSE.json())
-        
-        
+       
         self.LATEST_VERSION: str = str(self.RESPONSE_JSON["info"]["version"])
         self.release: list = list(self.RESPONSE_JSON["releases"][self.LATEST_VERSION])
-        
-        self.download_urls: list[str] = []
+        print(self.release)
+        self.desiredInfo: list[dict[str, str]] = []
 
         for _, release_info in enumerate(self.release):
-            # print(release_info)
             if release_info["packagetype"] == "bdist_wheel":
-                self.download_urls.append(release_info["url"])
+                self.desiredInfo.append({"pythonVersion":release_info["python_version"], "fileName":release_info["filename"], "downloadUrl":release_info["url"]})
+        
+        self.currentOs = platform.system().lower().replace("windows", "win")
+        self.currentPYversion = f"cp{sys.version_info.major}{sys.version_info.minor}"
+        self.check_compatibility(self.currentOs, self.currentPYversion)
 
-        print(self.download_urls)
+
+    def check_compatibility(self, currentOs: str, currentPYversion: str) -> None:
+        for _, info in enumerate(self.desiredInfo):
+            print(info)
+            if (currentPYversion in info["pythonVersion"] or "py3" in info["pythonVersion"]) and (currentOs in info["fileName"] or "any" in info["fileName"].replace(".", "-").split("-")):
+                print(info)
+                print("compatible")
 
 
 if __name__ == "__main__":
     try:
         PIP: pip_handler = pip_handler("requests")
-    except KeyError:
-        print("OOPS, looks like there is no package found !")
 
+    except KeyError:
+        print("OOPS, looks like there is no package found ! ")
