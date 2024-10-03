73abe6d6f582adf7adde8ad98f539901da8fd5d5
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-03 17:03:11 +0330

    downloading the tarball + abstracting user control over the class object

--- npm.py
+++ npm.py
@@ -1,23 +1,34 @@
-import platform
 import requests
-import sys
 
 
 class npm_handeler:
     def __init__(self, packageName: str) -> None:
         self.URL: str = f"https://registry.npmjs.org/{packageName}"
-        self.__get_download_link()
+        self.TARBALL_URL: str = self.__get_download_link()
 
-    def __get_download_link(self) -> None:
-        self.RESPONSE: requests.Response = requests.get(self.URL)
-        self.RESPONSE_JSON: dict = dict(self.RESPONSE.json())
+        self.__download(self.TARBALL_URL)
 
-        self.LATEST_VERSION: str = self.RESPONSE_JSON["dist-tags"]["latest"]
-        self.LATEST_INFO: dict = dict(self.RESPONSE_JSON["versions"][self.LATEST_VERSION])
+    def __get_download_link(self) -> str:
+        RESPONSE: requests.Response = requests.get(self.URL)
+        RESPONSE_JSON: dict = dict(RESPONSE.json())
+
+        LATEST_VERSION: str = RESPONSE_JSON["dist-tags"]["latest"]
+        LATEST_INFO: dict = dict(RESPONSE_JSON["versions"][LATEST_VERSION])
         
-        self.TARBALL_URL: str = self.LATEST_INFO["dist"]["tarball"]
+        TARBALL_URL: str = LATEST_INFO["dist"]["tarball"]
 
-        print(self.TARBALL_URL)
+        return TARBALL_URL
+
+    def __download(self, downloadUrl: str) -> None:
+        RESPONSE: requests.Response = requests.get(downloadUrl)
+        RESPONSE_CONTENT: bytes = RESPONSE.content
+        
+        FILE_NAME: str = downloadUrl.split("/")[-1]
+
+        with open(FILE_NAME, "wb") as file:
+            file.write(RESPONSE_CONTENT)
+
+        print("downloaded !")
 
 
 if __name__ == "__main__":
