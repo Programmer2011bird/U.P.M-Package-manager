e0077261eac6f28a1d26eb8cdbf9c77f937c5f62
Programmer2011bird <maysamshaker7@gmail.com>
2024-10-07 00:59:17 +0330

    #

--- decompressor.py
+++ decompressor.py
@@ -18,9 +18,3 @@ class extractTarball:
     def __decompress(self, path: str, output_dir: str):
         with tarfile.open(path, "r") as tar:
             tar.extractall(output_dir)
-
-
-if __name__ == "__main__":
-    wheel_extractor: extractWheel = extractWheel("Test_Files/requests-2.32.3-py3-none-any.whl", "Test_Files/requests")
-    tarball_extractor: extractTarball = extractTarball("Test_Files/express-4.21.0.tgz", "Test_Files/express")
-    
